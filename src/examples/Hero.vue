333c986fa4c835702f30203644777f15d61acd0f
AndrewGudi <jamesbanner1225@gmail.com>
2022-11-15 09:48:01 +0200

    fix errors

--- src/examples/Hero.vue
+++ src/examples/Hero.vue
@@ -94,12 +94,6 @@
 export default {
   name: 'Hero',
   created() {
-    $(".down-arr").click(function () {
-      $('html, body').animate({
-        scrollTop: $(document).height()
-      }, 'slow');
-      return false;
-    });
 
   }
 }
