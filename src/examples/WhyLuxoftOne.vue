b16b272a7dcf7a1e805c286704ddbc84f7bb93e8
DDziubenko <hedgrin@gmail.com>
2022-11-15 12:52:40 +0200

    remake to bootstrap

--- src/examples/WhyLuxoftOne.vue
+++ src/examples/WhyLuxoftOne.vue
@@ -1,55 +1,62 @@
 <template>
   <div>
-    <section class="why-luxoft why-luxoft--cards-4">
-      <h2 class="why-luxoft__title">Why Luxoft</h2>
-      <div class="why-luxoft__row">
-        <div class="row__col">
-          <h4 class="col__title">Technological independence and strong partnerships</h4>
-          <p class="col__desc">While accelerating growth we solve your legacy-system issues, protecting existing
-            investments with technologically independent solutions and the support of our top-tier partner network.</p>
-          <span class="col__arrows"> >>> </span>
+    <section class="container-why-luxoft-optional container-xxl">
+      <h2 class="title">Why Luxoft</h2>
+      <div class="row">
+          <div class="col">
+            <a href="#">
+              <h5>Technological independence and strong partnerships</h5>
+              <p>While accelerating growth we solve your legacy-system issues, protecting existing
+                investments with technologically independent solutions and the support of our top-tier partner network.</p>
+            </a>
+          </div>
+        <div class="col">
+          <a href="#">
+            <h5>Technological independence and strong partnerships</h5>
+            <p>While accelerating growth we solve your legacy-system issues, protecting existing
+              investments with technologically independent solutions and the support of our top-tier partner network.</p>
+          </a>
         </div>
-        <div class="row__col">
-          <h4 class="col__title">Technological independence and strong partnerships</h4>
-          <p class="col__desc">While accelerating growth we solve your legacy-system issues, protecting existing
-            investments with technologically independent solutions and the support of our top-tier partner network.</p>
-          <span class="col__arrows"> >>> </span>
+        <div class="col">
+          <a href="#">
+            <h5>Technological independence and strong partnerships</h5>
+            <p>While accelerating growth we solve your legacy-system issues, protecting existing
+              investments with technologically independent solutions and the support of our top-tier partner network.</p>
+          </a>
         </div>
-        <div class="row__col">
-          <h4 class="col__title">Technological independence and strong partnerships</h4>
-          <p class="col__desc">While accelerating growth we solve your legacy-system issues, protecting existing
-            investments with technologically independent solutions and the support of our top-tier partner network.</p>
-          <span class="col__arrows"> >>> </span>
-        </div>
-        <div class="row__col">
-          <h4 class="col__title">Technological independence and strong partnerships</h4>
-          <p class="col__desc">While accelerating growth we solve your legacy-system issues, protecting existing
-            investments with technologically independent solutions and the support of our top-tier partner network.</p>
-          <span class="col__arrows"> >>> </span>
+        <div class="col">
+          <a href="#">
+            <h5>Technological independence and strong partnerships</h5>
+            <p>While accelerating growth we solve your legacy-system issues, protecting existing
+              investments with technologically independent solutions and the support of our top-tier partner network.</p>
+          </a>
         </div>
       </div>
     </section>
 
-    <section class="why-luxoft why-luxoft--cards-3">
-      <h2 class="why-luxoft__title">Why Luxoft</h2>
-      <div class="why-luxoft__row">
-        <div class="row__col">
-          <h4 class="col__title">Technological independence and strong partnerships</h4>
-          <p class="col__desc">While accelerating growth we solve your legacy-system issues, protecting existing
-            investments with technologically independent solutions and the support of our top-tier partner network.</p>
-          <span class="col__arrows"> >>> </span>
+    <section class="container-why-luxoft-optional container-xxl">
+      <h2 class="title">Why Luxoft</h2>
+      <div class="row">
+        <div class="col">
+          <a href="#">
+            <h5>Technological independence and strong partnerships</h5>
+            <p>While accelerating growth we solve your legacy-system issues, protecting existing
+              investments with technologically independent solutions and the support of our top-tier partner network.</p>
+          </a>
         </div>
-        <div class="row__col">
-          <h4 class="col__title">Technological independence and strong partnerships</h4>
-          <p class="col__desc">While accelerating growth we solve your legacy-system issues, protecting existing
-            investments with technologically independent solutions and the support of our top-tier partner network.</p>
-          <span class="col__arrows"> >>> </span>
+        <div class="col">
+          <a href="#">
+            <h5>Technological independence and strong partnerships</h5>
+            <p>While accelerating growth we solve your legacy-system issues, protecting existing
+              investments with technologically independent solutions and the support of our top-tier partner network.</p>
+          </a>
         </div>
-        <div class="row__col">
-          <h4 class="col__title">Technological independence and strong partnerships</h4>
-          <p class="col__desc">While accelerating growth we solve your legacy-system issues, protecting existing
-            investments with technologically independent solutions and the support of our top-tier partner network.</p>
-          <span class="col__arrows"> >>> </span>
+        <div class="col">
+          <a href="#">
+            <h5>Technological independence and strong partnerships</h5>
+            <p>While accelerating growth we solve your legacy-system issues, protecting existing
+              investments with technologically independent solutions and the support of our top-tier partner network.</p>
+          </a>
         </div>
       </div>
     </section>
