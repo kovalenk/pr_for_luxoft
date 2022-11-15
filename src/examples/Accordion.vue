858825d2025cfe2c4aef3d45d4f2f665350bac6b
AndrewGudi <jamesbanner1225@gmail.com>
2022-11-15 19:21:06 +0200

    DGTLDEV-423784 - add bootstrap accordion

--- src/examples/Accordion.vue
+++ src/examples/Accordion.vue
@@ -1,54 +1,97 @@
 <template>
   <div>
-    <section class="accordion-section">
-      <div class="accordion-section__container">
-        <h2 class="container__title">The desire to support proactive brushing</h2>
-        <p class="container__article">The Philips Sonicare team, makers of the #1 recommended brand by dental
-          professionals, wanted to build upon the success of their rechargeable power toothbrush for kids. By
-          harnessing the latest in Bluetooth connectivity, there was an opportunity to create a holistic brushing
-          experience for kids using a Philips Sonicare brush that was so compelling that they would proactively ask to
-          brush their teeth, while learning proper brushing techniques that would benefit their health for decades to
-          come.
-        </p>
-        <div class="container__accordion">
-          <div class="accordion__row">
-            <h4 class="row__title">System testing and validation</h4>
-            <p class="row__desc">While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.</p>
+    <section class="container-accordion container-accordion--black">
+      <h2 class="container-accordion--title">The desire to support proactive brushing</h2>
+      <p class="container-accordion--article">The Philips Sonicare team, makers of the #1 recommended brand by dental
+        professionals, wanted to build upon the success of their rechargeable power toothbrush for kids. By
+        harnessing the latest in Bluetooth connectivity, there was an opportunity to create a holistic brushing
+        experience for kids using a Philips Sonicare brush that was so compelling that they would proactively ask to
+        brush their teeth, while learning proper brushing techniques that would benefit their health for decades to
+        come.
+      </p>
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOneBlack">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOneBlack" aria-expanded="false" aria-controls="collapseOneBlack">
+              System testing and validation
+            </button>
+          </h2>
+          <div id="collapseOneBlack" class="accordion-collapse collapse" aria-labelledby="headingOneBlack" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.
+            </div>
           </div>
-          <div class="accordion__row">
-            <h4 class="row__title">Bluetooth connectivity</h4>
-            <p class="row__desc">While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.</p>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwoBlack">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwoBlack" aria-expanded="false" aria-controls="collapseTwoBlack">
+              Bluetooth connectivity
+            </button>
+          </h2>
+          <div id="collapseTwoBlack" class="accordion-collapse collapse" aria-labelledby="headingTwoBlack" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.
+            </div>
           </div>
-          <div class="accordion__row">
-            <h4 class="row__title">Philips Sonicare brush</h4>
-            <p class="row__desc">While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.</p>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThreeBlack">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThreeBlack" aria-expanded="false" aria-controls="collapseThreeBlack">
+              Philips Sonicare brush
+            </button>
+          </h2>
+          <div id="collapseThreeBlack" class="accordion-collapse collapse" aria-labelledby="headingThreeBlack" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.
+            </div>
           </div>
         </div>
       </div>
     </section>
-
-    <section class="accordion-section accordion-section--white">
-      <div class="accordion-section__container">
-        <h2 class="container__title">The desire to support proactive brushing</h2>
-        <p class="container__article">The Philips Sonicare team, makers of the #1 recommended brand by dental
-          professionals, wanted to build upon the success of their rechargeable power toothbrush for kids. By
-          harnessing the latest in Bluetooth connectivity, there was an opportunity to create a holistic brushing
-          experience for kids using a Philips Sonicare brush that was so compelling that they would proactively ask to
-          brush their teeth, while learning proper brushing techniques that would benefit their health for decades to
-          come.
-        </p>
-        <div class="container__accordion">
-          <div class="accordion__row">
-            <h4 class="row__title">System testing and validation</h4>
-            <p class="row__desc">While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.</p>
+    <section class="container-accordion container-accordion--white">
+      <h2 class="container-accordion--title">The desire to support proactive brushing</h2>
+      <p class="container-accordion--article">The Philips Sonicare team, makers of the #1 recommended brand by dental
+        professionals, wanted to build upon the success of their rechargeable power toothbrush for kids. By
+        harnessing the latest in Bluetooth connectivity, there was an opportunity to create a holistic brushing
+        experience for kids using a Philips Sonicare brush that was so compelling that they would proactively ask to
+        brush their teeth, while learning proper brushing techniques that would benefit their health for decades to
+        come.
+      </p>
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOneWhite">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOneWhite" aria-expanded="false" aria-controls="collapseOneWhite">
+              System testing and validation
+            </button>
+          </h2>
+          <div id="collapseOneWhite" class="accordion-collapse collapse" aria-labelledby="headingOneWhite" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.
+            </div>
           </div>
-          <div class="accordion__row">
-            <h4 class="row__title">Bluetooth connectivity</h4>
-            <p class="row__desc">While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.</p>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwoWhite">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwoWhite" aria-expanded="false" aria-controls="collapseTwoWhite">
+              Bluetooth connectivity
+            </button>
+          </h2>
+          <div id="collapseTwoWhite" class="accordion-collapse collapse" aria-labelledby="headingTwoWhite" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.
+            </div>
           </div>
-          <div class="accordion__row">
-            <h4 class="row__title">Philips Sonicare brush</h4>
-            <p class="row__desc">While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.</p>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThreeWhite">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThreeWhite" aria-expanded="false" aria-controls="collapseThreeWhite">
+              Philips Sonicare brush
+            </button>
+          </h2>
+          <div id="collapseThreeWhite" class="accordion-collapse collapse" aria-labelledby="headingThreeWhite" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              While accelerating growth we solve your legacy-system issues, protecting existing investments with technologically independent solutions and the support of our top-tier partner network.
+            </div>
           </div>
         </div>
       </div>
