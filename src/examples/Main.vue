5857dc4ce859cdc66607281fde7490d2bbff7db1
pronchik <pronmaxim2001@gmail.com>
2022-11-16 15:21:05 +0200

    DGTLDEV-423785

--- src/examples/Main.vue
+++ src/examples/Main.vue
@@ -23,46 +23,46 @@
 
 <script>
 
-import Industries from "@/examples/Industries";
-import WhyLuxoftOne from "@/examples/WhyLuxoftOne";
-import WhyLuxoftTwo from "@/examples/WhyLuxoftTwo";
-import KeyPoints from "@/examples/KeyPoints";
-import HowWeCanHelp from "@/examples/HowWeCanHelp";
-import Partnership from "@/examples/Partnership";
-import SectionWithIcons from "@/examples/SectionWithIcons";
-import Team from "@/examples/Team";
+// import Industries from "@/examples/Industries";
+// import WhyLuxoftOne from "@/examples/WhyLuxoftOne";
+// import WhyLuxoftTwo from "@/examples/WhyLuxoftTwo";
+// import KeyPoints from "@/examples/KeyPoints";
+// import HowWeCanHelp from "@/examples/HowWeCanHelp";
+// import Partnership from "@/examples/Partnership";
+// import SectionWithIcons from "@/examples/SectionWithIcons";
+// import Team from "@/examples/Team";
 import Breadcrumbs from "@/examples/Breadcrumbs";
-import Video from "@/examples/Video";
-import HowWeHelp from "@/examples/HowWeHelp";
-import Hero from "@/examples/Hero";
-import Benefits from "@/examples/Benefits";
-import Accordion from "@/examples/Accordion";
-import Solutions from "@/examples/Solutions";
-import CaseStudies from "@/examples/CaseStudies";
-import Insights from "@/examples/Insights";
-import Awards from "@/examples/Awards";
+// import Video from "@/examples/Video";
+// import HowWeHelp from "@/examples/HowWeHelp";
+// import Hero from "@/examples/Hero";
+// import Benefits from "@/examples/Benefits";
+// import Accordion from "@/examples/Accordion";
+// import Solutions from "@/examples/Solutions";
+// import CaseStudies from "@/examples/CaseStudies";
+// import Insights from "@/examples/Insights";
+// import Awards from "@/examples/Awards";
 
 export default {
   name: 'main-name',
   components: {
-    Industries,
-    WhyLuxoftOne,
-    WhyLuxoftTwo,
-    KeyPoints,
-    HowWeCanHelp,
-    Partnership,
-    SectionWithIcons,
-    Team,
+    // Industries,
+    // WhyLuxoftOne,
+    // WhyLuxoftTwo,
+    // KeyPoints,
+    // HowWeCanHelp,
+    // Partnership,
+    // SectionWithIcons,
+    // Team,
     Breadcrumbs,
-    Video,
-    HowWeHelp,
-    Hero,
-    Benefits,
-    Accordion,
-    Solutions,
-    CaseStudies,
-    Insights,
-    Awards,
+    // Video,
+    // HowWeHelp,
+    // Hero,
+    // Benefits,
+    // Accordion,
+    // Solutions,
+    // CaseStudies,
+    // Insights,
+    // Awards,
   }
 }
 </script>
